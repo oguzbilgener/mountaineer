277f1dbd298e0838c3690fb0093b65a93bfeb973
Oguz Bilgener <oguzbilgener@gmail.com>
2017-05-27 17:56:58 +0300

    fix

--- btxb2/btxb2.ino
+++ btxb2/btxb2.ino
@@ -52,7 +52,7 @@ void setup() {
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
 
-  IS_LEADER = digitalRead(LEADERPIN)
+  IS_LEADER = digitalRead(LEADERPIN);
 
   if (pressure.begin()) {
        // Serial.println("BMP180 init success");
